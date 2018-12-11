8e6ca1e294105f2e931fb5ad2e0e84b103440e90
Papibarozza <mildner.arvid@gmail.com>
2018-12-11 16:11:24 +0100

    PCA added

--- arvid.m
+++ arvid.m
@@ -1,37 +1,32 @@
-a = prnist([0:9],[1:40:1000]);
+a = prnist([0:9],[1:5:1000]);
 
 %%
-train = a(2:2:250)
-test = a(1:2:250);
-%x = im_features(test,test,{'Area','Perimeter','Eccentricity'});
-
-%test = seldat(test,[1:2]); % select the classes 3,6,9,12,15 and 18
-%preproc = im_box([],0,1)*im_gray*im_rotate*im_resize([],[30 30])*im_box([],2,0); %Preprocess images
-preproc = im_box([],0,1)*im_gray*im_rotate*im_resize([],[20 20])*im_box([],2,0);
-figure(2); show(test*preproc);
-showfigs
-train = train*preproc;
-test = test*preproc;
+%BEST PERFORMANCE WITH [16 16] and [PCMA(raw) sillhouette_features]  only (10%)
+preproc = im_box([],0,1)*im_gray*im_resize([],[16 16])*im_box([],2,0);
+%preproc = im_box([],0,1)
+a = a*preproc;
 %%
-feature_selected = {'Eccentricity','Area','Orientation','FilledArea'};
-trained_features = im_features(train,train,feature_selected);
-test_features = im_features(test,test,feature_selected);
-scatterd(trained_features)
+all_features_im = im_features(a,a,'all');
 
 %%
-c_1 = knnc(trained_features(:,1:2),5)*classc;
-c_2 = knnc(trained_features(:,3:4),5)*classc;
-%classifier = knnc(pr_dataset(train),5)
-c_3 = knnc(pr_dataset(train),5)*classc;
-c_4 = pr_dataset(train)*svc*classc;
-
-combined_classifier = [c_3;c_4]*meanc
+pcam_im_features_mapping = pcam(prdataset(all_features_im,[]),18);
 %%
-%d_classifier*testc
-%pr_dataset(test)*classifier*testc
-t_1 = test_features(:,1:2)
-t_2 =test_features(:,3:4)
-t_3 = pr_dataset(test);
-t_3*c_4*testc
-t_2*c_2*testc
-%[t_3 t_3]*combined_classifier*testc
+pcam = pcam(prdataset(a,[]));
+feats = prdataset(a,[])*pcam;
+%%
+feat_im = prdataset(all_features_im)*pcam_im_features_mapping
+feats_all = prdataset([+feats +feat_im],getlabels(a))
+%%
+feat_im_moments = im_moments(prdataset(a),'zer',10);
+%%
+feat_im_profile = im_profile(a);
+%%
+im_profile_mapping = pcam(feat_im_profile,18);
+%%
+feat_im_profile = feat_im_profile*im_profile_mapping;
+%%
+%v = knnc([],3)
+%v = [nmc*classc ldc*classc qdc*classc]*fisherc
+v2 = [parzenc([],0.5) knnc([],3)]*ldc
+test = prdataset([+feats +prdataset(feat_im_profile)],getlabels(a))
+[Err, Cerr] = prcrossval(test,v,10);
