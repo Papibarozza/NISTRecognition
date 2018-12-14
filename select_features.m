e34402de2ad2de51edea6efd3b0aa3a74df042ba
Jakob <jakobskok@hotmail.com>
2018-12-14 17:11:24 +0100

    Added feature selection

--- select_features.m
+++ select_features.m
@@ -1,3 +1,4 @@
 function [new_feature_space] = select_best_features(a,features_needed)
+%select the best features in a picture representation
 [feature_mapping,r] = featself(a,'eucl-m',features_needed)
 new_feature_space = a*feature_mapping
